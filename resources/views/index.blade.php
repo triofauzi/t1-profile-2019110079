ada83641b00f1f9aa8c5702439549f8fc16a9e99
Trio fauzi nugraha <triofauzinugraha@gmail.com>
2022-09-22 01:51:53 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,125 @@
+<!Doctype html>
+<html>
+
+
+    <head>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+
+        <title>Home</title>
+
+
+
+
+      </head>
+
+
+
+ <body >
+    <nav class="navbar navbar-light navbar-expand-md bg-white justify-content-center fixed-top">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <h1 class="font_0 mx-auto order-100" style="font-size:25px;">
+             TRIO FAUZI N.
+            </h1>
+        </div>
+
+
+     <div class="navbar-collapse collapse w-100 order-2 dual-collapse2">
+            <ul class="navbar-nav mx-auto order-0">
+                <li class="nav-item">
+                    <a class="nav-link text-dark"  aria-current="page" href="portfolio.php">portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  aria-current="page" href="#">Home</a>
+                </li>
+            </ul>
+     </div>
+    </nav>
+
+    <br><br><br>
+    <div class="jumbotron" id="jumbotron" >
+        <div class="container">
+            <div class="text-center" >
+                <img src="img/gambar.jpeg" class="rounded-circle img-thumbnail" >
+          <h1 class="font_2" style="font-size:30px;"> Trio Fauzi Nugraha</h1>
+          <h3 class="lead" >2019110079</h3>
+          </div>
+        </div>
+      </div>
+
+    <br><br><br>
+    <section class="about" id="about"  >
+     <div class="container" >
+        <div class="row justify-content-center" >
+          <div class="col-lg-8  text-danger" >
+            <h3 class="font_3 " style="line-height:1.4em; font-size:40px;">
+              <span  class="font_3"  style="letter-spacing:0.01em;">Hi, I'm Trio Fauzi.
+               <br>
+               I'm a Student at STMIK Likmi Semester Seven, Majoring in Informatics Management.
+              </span>
+               <br>
+              &nbsp;
+            </h3>
+          </div>
+        </div>
+
+        <div class="row justify-content-center" >
+         <div class="col-lg-6" >
+          <div class="col text-center" id="comp-jupv6vmj" class="_1Q9if _3bcaz" data-testid="richTextElement">
+            <h2 class="font_2" style="font-size:30px;">ABOUT ME</h2>
+          </div>
+         </div>
+        </div>
+        <br>
+        <div class="row justify-content-center" >
+            <div class="col-lg-4" >
+            <ul class="font_8" style="font-size:16px;">
+                <li>
+                 <p class="font_8" style="line-height:1.8em; font-size:16px;">Saya lahir di Garut tahun 2001,sekarang saya tinggal di kota Bandung karena sedang berkuliah di kota bandung&nbsp;</p>
+                </li>
+                <li>
+                 <p class="font_8" style="line-height:1.8em; font-size:16px;">Hobi saya berolahraga&nbsp;nonton musik dan trevelling&nbsp;</p>
+                </li>
+                <li>
+                 <p class="font_8" style="line-height:1.8em; font-size:16px;">Saya menguasai beberapa bahasa pemrograman HTML, CSS, JavaScript dan PHP&nbsp;</p>
+                </li>
+            </ul>
+            </div>
+            <div class="col-md-4" >
+                <ul class="font_8" style="font-size:16px;">
+                    <li>
+                    <p class="font_8" style="line-height:1.8em; font-size:16px;"> (usability test, field study, remote studies,&nbsp;interviews, surveys, tree test, card sort)</p>
+                    </li>
+                    <li>
+                    <p class="font_8" style="line-height:1.8em; font-size:16px;">Experience&nbsp;in conducting user research for&nbsp;different customer-facing products for the web app, mobile apps (iOS &amp; Android) and internal tool</p>
+                    </li>
+                    </ul>
+                </div>
+
+      </div>
+    </section>
+    <br><br>
+    <footer class="bg-light text-danger mt-5" >
+        <div class="container" >
+            <div class="row" >
+                <div class="col text-center" >
+                    <p>2022 @triofauzin</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+  crossorigin="anonymous"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+ </body>
+</html>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,151 @@
+<!Doctype html>
+<html>
+
+
+    <head>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
+              <link rel="stylesheet" type="text/css" href="css/style.css">
+
+        <title>Home</title>
+
+
+
+
+
+    </head>
+
+
+
+<body >
+    <nav class="navbar navbar-light navbar-expand-md bg-white justify-content-center fixed-top">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <h1 class="font_0 mx-auto order-100" style="font-size:25px;">
+             TRIO FAUZI N.
+            </h1>
+        </div>
+
+
+        <div class="navbar-collapse collapse w-100 order-2 dual-collapse2">
+            <ul class="navbar-nav mx-auto order-0">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="portfolio.blade.php">portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <br><br><br>
+    <div class="jumbotron" id="jumbotron" >
+        <div class="container">
+            <div class="text-center" >
+                <img src="img/gambar.jpeg" class="rounded-circle img-thumbnail" >
+          <h1 class="font_2" style="font-size:30px;"> Trio Fauzi Nugraha</h1>
+          <h3 class="lead" >2019110079</h3>
+          </div>
+        </div>
+      </div>
+
+      <br><br>
+      <section class="about" id="about"  >
+       <div class="container" >
+          <div class="row justify-content-center" >
+            <div class="col-lg-8 text-center text-danger" >
+              <h3 class="font_3 " style="line-height:1.4em; font-size:40px;">
+                <span  class="font_3"  style="letter-spacing:0.01em;">Hello, Welcome To My Gallery.
+                 <br>
+                 Enjoy With My Creation.
+                </span>
+                 <br>
+                &nbsp;
+              </h3>
+            </div>
+          </div>
+
+          <section class="portfolio bg-light " id="portfolio" >
+			<div class="container" >
+				<div class="row pt-5 mb-4" >
+					<div class="col text-center" >
+						<h2>Portfolio</h2>
+					</div>
+				</div>
+				<div class="row" >
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/GAMBAR1.jpg" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Ini merupakan hasil dari seni rupa kontenporer.</p>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/gambar10.png" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Karya ini merupakan hasil karya dari sir dandy beliau adalah musisi dan senimasan ilustrator.</p>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/gambar11.jpg" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Karya ini merupakan hasil karya dari sir dandy beliau adalah musisi dan senimasan ilustrator..</p>
+						  </div>
+						</div>
+					</div>
+				</div>
+				<div class="row" >
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/images1.jpg" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Karya ini merupakan hasil karya dari sir dandy beliau adalah musisi dan senimasan ilustrator..</p>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/images2.jpg" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Karya ini merupakan hasil karya dari sir dandy beliau adalah musisi dan senimasan ilustrator..</p>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md mb-4" >
+						<div class="card">
+						  <img src="img/gambar5.jpg" class="card-img-top">
+						  <div class="card-body">
+						    <p class="card-text">Ini merupakan cover album dari band the upstairs, lagu “Matraman” sendiri dinobatkan sebagai salah satu dari 150 lagu Indonesia Terbaik Sepanjang Masa versi majalah Rolling Stone Indonesia .</p>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+        <br><br>
+     <footer class="bg-light text-danger mt-5" >
+        <div class="container" >
+            <div class="row" >
+                <div class="col text-center" >
+                    <p>2022 @triofauzin</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
